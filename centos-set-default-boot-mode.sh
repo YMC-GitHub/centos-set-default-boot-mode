e7ed36357360c25a29da059e4f25670d49b6c9cb
yemiancheng <ymc.github@gmail.com>
2019-11-19 14:01:18 +0800

    refactor(core): renaming file

--- centos-set-default-boot-mode.sh
+++ centos-set-default-boot-mode.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 # centos设置默认启动模式
+# 需要：
+# 删除旧的配置
+# 创建新的配置
+# 重启启动电脑
 
-BOOT_MODE=graphical #"multi-user|graphical"
+BOOT_MODE=multi-user #"multi-user|graphical"
+#BOOT_MODE=multi-user
 
-::<<centos7-set-default-boot-mode1
+:: <<centos7-set-default-boot-mode
 # centos启用图形模式
 # cat /etc/inittab
 # systemctl set-default graphical.target
@@ -13,30 +18,29 @@ BOOT_MODE=graphical #"multi-user|graphical"
 # systemctl get-default
 # shutdown -r now
 # /etc/systemd/system/default.target
-centos7-set-default-boot-mode1
-
+centos7-set-default-boot-mode
+git mv centos-set-default-mode.sh centos-set-default-boot-mode.sh
 #删除配置
 rm -rf /etc/systemd/system/default.target
 #创建配置
 #2命令模式
-function boot_as_cli_mode(){
+function boot_as_cli_mode() {
   ln -sf /lib/systemd/system/multi-user.target /etc/systemd/system/default.target
 }
 #2图形模式
-function boot_as_gui_mode(){
-ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
-} 
+function boot_as_gui_mode() {
+  ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+}
 case $BOOT_MODE in
-    'multi-user')
-     boot_as_cli_mode
-    ;;
-    'graphical')
-     boot_as_gui_mode
-    ;;
+'multi-user')
+  boot_as_cli_mode
+  ;;
+'graphical')
+  boot_as_gui_mode
+  ;;
 esac
 #重启电脑
 reboot
-
 #### 参考文献
 # vmware CentOS7图形界面与命令行界面切换
 # https://blog.csdn.net/m0_37696990/article/details/81750849
